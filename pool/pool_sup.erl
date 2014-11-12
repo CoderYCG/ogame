f4df163de7d24cbcad6d29f40a7a0e015621a36d
coder_yu <1062502052@qq.com>
2014-11-12 19:51:06 +0800

    add pool supervisor

---
+++ pool/pool_sup.erl
@@ -0,0 +1,26 @@
+%%-----------------------------------------------------
+%% @Module	:	pool_sup
+%% @Description	:	monitor the process where in the pool
+%%-----------------------------------------------------
+-module(pool_sup).
+-behaviour(supervisor).
+-export([start_link/1, init/1]).
+
+start_link(Mod, Args) ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, {Mod, Args}).
+
+
+init({Mod, Args}) ->
+	RestartStrateg = {simple_one_for_one, 0, 1},
+	ChildSpec =
+	[
+		{
+			Mod,
+			{Mod, start_link, [Args]},
+			temporary,
+			5000,
+			worker,
+			[Mod]
+		}
+	],
+	{ok, {RestartStrateg, ChildSpec}}.
