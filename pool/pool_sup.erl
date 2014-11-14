1fd6783ed40a3a1ec844e30f1a096ff1bb2373a3
coder_yu <1062502052@qq.com>
2014-11-14 17:48:03 +0800

    添加进程池管理

--- pool/pool_sup.erl
+++ pool/pool_sup.erl
@@ -4,13 +4,13 @@
 %%-----------------------------------------------------
 -module(pool_sup).
 -behaviour(supervisor).
--export([start_link/1, init/1]).
+-export([start_link/2, init/1]).
 
 start_link(Mod, Args) ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, {Mod, Args}).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, [Mod, Args]).
 
 
-init({Mod, Args}) ->
+init([Mod, Args]) ->
 	RestartStrateg = {simple_one_for_one, 0, 1},
 	ChildSpec =
 	[
